aee7fdc6119d3c348349905d217f23e99e944d06
vaibhavsarswat <22csaiml035@jssaten.ac.in>
2025-04-24 19:36:46 +0530

    sim sim

--- tf/main.tf
+++ tf/main.tf
@@ -176,7 +176,7 @@ resource "aws_ecs_cluster" "main" {
 }
 
 resource "aws_iam_role" "new_quasar_ecs_task_execution_role" {
-  name = "new_quasar_ecsTaskExecutionRole7"
+  name = "new_quasar_ecsTaskExecutionRole11"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
